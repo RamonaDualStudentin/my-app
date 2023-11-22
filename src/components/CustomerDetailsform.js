52aea396929cf8690ac38208a8fa2aa260089af3
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2023-11-22 13:27:31 +0000

    change sizes of boxes and positioning

--- src/components/CustomerDetailsform.js
+++ src/components/CustomerDetailsform.js
@@ -17,62 +17,80 @@ function CustomerDetailsform() {
 
       <div className="Full Name">
         <label htmlFor="full-name">Full Name*</label>
+
         <div className="name-fields">
+          <label htmlFor='First Name'>First Name </label>
           <input type="text" id="first-name" name="first-name" placeholder="First Name" required />
+          <label htmlFor='Last Name'>Last Name </label>
           <input type="text" id="last-name" name="last-name" placeholder="Last Name" required />
         </div>
       </div>
       <div className="address">
         <label htmlFor="address">Address*</label>
 
-        <div className="adress-fields">
+        <div className="address-fields">
+
+          <label htmlFor="Street Address">Street Address* </label>
+          <input type="text" id="Street Address" name="Street Address" placeholder="Street Address" required />
 
 
-          <label>Street Address</label>
-          <input type="text" placeholder="Street Address" />
-          <label>Street Address Line 2:</label>
-          <input type="text" placeholder="Street Address Line 2" />
-          <label>City</label>
-          <input type="text" placeholder="City" />
-          <label>State/Province</label>
-          <input type="text" placeholder="State/Province" />
-          <label>Postal / Zip Code</label>
-          <input type="text" placeholder="Postal / Zip Code" />
+
+          <label>Street Address Line 2 </label>
+          <input type="text" id="Street Address Line 2" name="Street Address Line 2" placeholder="Street Address Line 2" required />
+
+          <label>City </label>
+          <input type="text" id="City" name="City" placeholder="City" required />
+
+          <label>State/Province </label>
+          <input type="text" id="State/Province" name="State/Province" placeholder="State/Province" required />
+
+          <label>Postal / Zip Code </label>
+
+          <input type="text" id="Postal / Zip Code" name="Postal / Zip Code" placeholder="Postal / Zip Code" required />
         </div>
 
         <div className="phone-number">
-          {/* Include phone and email components here */}
+          <label>Phone Number* </label>
+
+          <input type="" id="phone-number" name="phone-number" placeholder="(000)000-0000" required />
         </div>
         <div className="email">
-          {/* Include phone and email components here */}
+          <label>Email </label>
+
+          <input type="text" id="email" name="email" placeholder="ex:email@yahoo.com" required />
         </div>
       </div>
 
       {/* Feedback Section */}
       <div className="feedback">
-        <label>How did you hear about us?</label>
-        <select>
+        <label>How did you hear about us?* </label>
+        <select >
           <option value="Please select">Please select</option>
-          {/* Add other options as needed */}
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          {/*add more if needed*/}
         </select>
 
         <div className="please-specify">
-          <label>Please Specify:</label>
+          <label>Please specify* </label>
           <input type="text" />
+
+
         </div>
 
         <div className="feedback-comment">
-          <label>Feedback about us:</label>
-          <textarea rows="48" />
+          <label>Feedback about us: </label>
+          <input type="text" />
         </div>
 
         <div className="suggestions">
-          <label>Suggestions for further improvement:</label>
-          <textarea rows="8" />
+          <label>Suggestions if any for further improvement: </label>
+          <input type="text" />
         </div>
 
         <div className="recommendation">
-          <label>Will you be willing to recommend us?</label>
+          <label>Will you be willing to recommend us? </label>
           <select>
             <option value="yes">Yes</option>
             <option value="no">No</option>
