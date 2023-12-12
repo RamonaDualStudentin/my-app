41f08b953e3299063163925291604d7b78885190
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2023-12-12 10:11:27 +0000

    2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <MenuBar />
-      <quickSelect />;
+      <quickSelect />
       <region />
       <regioncards />
       <serviceRequest />
