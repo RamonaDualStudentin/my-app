96b5a78cc17a9da683062619b7cea5ed164ef2e1
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2023-12-12 13:05:05 +0000

    switch on off

--- src/components/quickSelect.js
+++ src/components/quickSelect.js
@@ -4,7 +4,14 @@ import "./styling for the components/quickSelect.css";
 function quickSelect() {
   return( <div className="quickSelect">
   <h1 className="quick-select-title">Hello,[Whatever User is Logged in] </h1>
-
+  <h2>Quick select</h2>
+  <p>
+    By default you'll only get results from your zone - turn it off and you'll see all zones
+  </p>
+  <div className="switch">
+  <input type="checkbox"/>
+  <span class="slider round"></span>
+  </div>
   <div>
     <input type="text" className="quick-select-search" placeholder="Search for assets by name=you'll be directed to the diagram view"/></div>
   
--- src/components/zone.js
+++ src/components/zone.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function zone() {
-  return (<div className="zone"></div>);
+  return (<div className="zone">
+    <h2>Select a zone</h2>
+    <input type="text" id="ZoneS"/>
+    <input type="text" id="Zone1"/>
+    <input type="text" id="ZoneN"/>
+    <input type="text" id="ZoneL"/>
+
+    
+
+
+  </div>);
 }
 
 export default zone;
