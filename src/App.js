15895721c9deca12cc77925d2238bff87c171b92
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2023-12-12 09:25:40 +0000

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React from 'react';
-import './App.css';
-import CustomerDetailsform from './components/CustomerDetailsform';
-import SubmitButton from './components/submitbutton';
-import Header from './components/header';
-import createTable from './components/table';
-
-
+import React from "react";
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <CustomerDetailsform />
-            <SubmitButton />
-            <createTable />
-
-
-
-
-        </div>
-    );
+  return <div className="App"></div>;
 }
 
 export default App;
