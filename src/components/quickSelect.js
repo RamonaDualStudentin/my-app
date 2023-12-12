06b69f46d75e156fd50ba1f164c6f4dbc940c5b7
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2023-12-12 11:17:57 +0000

    quickselect css beginning

--- src/components/quickSelect.js
+++ src/components/quickSelect.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./styling for the components/quickSelect.css";
 
 function quickSelect() {
-  return( <div className="quickSelect"></div>);
+  return( <div className="quickSelect">
+  <h1 className="quick-select-title">Hello,[Whatever User is Logged in] </h1>
+
+  <div>
+    <input type="text" className="quick-select-search" placeholder="Search for assets by name=you'll be directed to the diagram view"/></div>
+  
+  
+  </div>);
 }
 
 export default quickSelect;
