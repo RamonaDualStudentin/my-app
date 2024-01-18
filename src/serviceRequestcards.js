458aa057c371f2f1b3f7afee6256b15fbc310736
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2024-01-18 09:45:52 +0000

    change

--- src/serviceRequestcards.js
+++ src/serviceRequestcards.js
@@ -6,11 +6,10 @@ function ServiceRequestCard({ name, date, status, zone, region }) {
     <div class="card">
     <div class="card-header">Substation Name</div>
     <div class="card-body">
-      <p><strong>Change detected:</strong> 08/06/23</p>
-      <p><a href="#" class="review-changes-link">Go to substation to review changes &rarr;</a></p>
+      <p><strong>Change detected:</strong> 08/06/23</p>              
       <p><strong>Zone:</strong> S - Region XYZ</p>
       <p><strong>Status:</strong> Pending</p>
-      <p><strong>Other balances affected:</strong> Yes</p>
+      <strong>Other balances affected:</strong>
     </div>
   </div>
   );
