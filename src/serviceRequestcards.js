8953518e61cb40bb23d79d8090fd683bfe645a26
RamonaDualStudentin <150044461+RamonaDualStudentin@users.noreply.github.com>
2024-01-18 09:25:08 +0000

    card component

--- src/serviceRequestcards.js
+++ src/serviceRequestcards.js
@@ -3,15 +3,16 @@ import "./styling for the components/serviceRequestcards.css";
 
 function ServiceRequestCard({ name, date, status, zone, region }) {
   return (
-    <div className="card-container">
-      <div className="card">
-        <h3>{name}</h3>
-        <p>Date: {date}</p>
-        <p>Status: {status}</p>
-        <p>Zone: {zone}</p>
-        <p>Region: {region}</p>
-      </div>
+    <div class="card">
+    <div class="card-header">Substation Name</div>
+    <div class="card-body">
+      <p><strong>Change detected:</strong> 08/06/23</p>
+      <p><a href="#" class="review-changes-link">Go to substation to review changes &rarr;</a></p>
+      <p><strong>Zone:</strong> S - Region XYZ</p>
+      <p><strong>Status:</strong> Pending</p>
+      <p><strong>Other balances affected:</strong> Yes</p>
     </div>
+  </div>
   );
 }
 
